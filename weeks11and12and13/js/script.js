8962d795fc666060e68d547e0a36d19b8109ca70
nkong008 <nkong2020@gmail.com>
2023-04-03 12:39:41 -0500

    added htm5 boiler plate and created some css, html, and javascript content.

---
+++ weeks11and12and13/js/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+$(function() {
+    gsap.registerPlugin(GSDevTools);
+    //targeting all sections 
+    let section = $("section");
+    //targeting css directly from this doc and adding some box shadows
+    section.css("box-shadow", "4px 4px 10px rgba(0, 0, 0, 0.4)")
+    console.log("section");
+
+    // creating animation time line 
+    let tl0 = gsap.timeline({
+        id: "animation0"
+
+    });
+    //where the animation is actually happening
+    tl0.from(section, {x: 100, opacity: 0, stagger: 0.5,})
+    GSDevTools.create({
+        animation0: "animation0"
+    });
+});
